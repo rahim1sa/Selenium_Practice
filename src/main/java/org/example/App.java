20c94aa3fc3414502a9996af94da20298c0a4a99
Rahim Isa <rahim.isa@outlook.com>
2022-08-16 00:01:24 +0200

    Verify Amazon Search Field tasks implemented

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+        VerifyAmazonSearchField vasf = new VerifyAmazonSearchField();
+        vasf.VerifyAmazonSearchField_01();
         System.out.println( "Hello World!" );
     }
 }
